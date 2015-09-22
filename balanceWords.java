087a8c9e7854000d36859ff02d0e99c3ddc92ffc
Josh Wein <jdog90000@yahoo.com>
2015-09-21 21:51:57 -0400

    Added balance words and garland text programs.

---
+++ balanceWords.java
@@ -0,0 +1,30 @@
+/**
+ * Today we're going to balance words on one of the letters in them. We'll use the position and letter itself to calculate the weight around the balance point. A word can be balanced if the weight on either side of the balance point is equal. Not all words can be balanced, but those that can are interesting for this challenge.
+ * The formula to calculate the weight of the word is to look at the letter position in the English alphabet (so A=1, B=2, C=3 ... Z=26) as the letter weight, then multiply that by the distance from the balance point, so the first letter away is multiplied by 1, the second away by 2, etc.
+ * @author Josh Wein
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        String[] words = {"STEAD", "UNINTELLIGIBILITY", "CONSUBSTANTIATION", "WRONGHEADED", "SUPERGLUE"};
+        for (String word : words) {
+            balanceWord(word);
+        }
+    }
+
+    static void balanceWord(String word) {
+        for (int mid = 0; mid < word.length(); mid++) {
+            int weight = 0;
+            for (int test = 0; test < word.length(); test++) {
+                if (mid != test) {
+                    weight += (word.charAt(test) - 64) * (mid - test);
+                }
+            }
+            if (weight == 0) {
+                System.out.println(word.substring(0, mid) + " " + word.charAt(mid) + " " + word.substring(mid + 1, word.length()));
+                return;
+            }
+        }
+        System.out.println(word + " does not balance");
+    }
+}
---
+++ garlandText.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+* A function garland that, given a lowercase word, returns the degree of the
+* word if it's a garland word, and 0 otherwise.
+*
+* @author Josh Wein
+*/
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File wordList = new File(.enable1.txt);
+        Scanner words = new Scanner(wordList);
+        String word = , max = ;        
+        int maxG = 0;
+        while (words.hasNextLine()) {            
+            word = words.nextLine();
+            int test = garland(word);
+            if(maxG  test){                
+                maxG = test;
+                max = word;
+                chain(word);
+            }
+        }
+        System.out.println(Max garland  + max +  -  + maxG);
+    }
+    static int garland(String word) {        
+        for (int pointer = 1; pointer  word.length(); pointer++) {
+            int temp = pointer, gCount, k;            
+            if (word.charAt(0) == word.charAt(pointer)) {
+               for(gCount = 1, k = pointer, pointer = 0; word.charAt(k) == word.charAt(pointer) && k != word.length()-1; pointer++, gCount++, k++){                   
+               }
+               if(k == word.length()-1 && word.charAt(k) == word.charAt(pointer)){
+                   return gCount;
+               }
+              
+            }
+            pointer = temp;
+        }
+        return 0;
+    }
+    
+    static void chain(String word){
+        int gar = garland(word);
+        System.out.print(word.substring(0, gar));
+        for(int i = 0; i  10; i++){
+            System.out.print(word.substring(gar, word.length()));
+        }
+        System.out.println();
+    }
+}
