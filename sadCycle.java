c3b6be7720535868ca6ff3869ab1f64e4bd0267e
Josh Wein <jdog90000@yahoo.com>
2015-09-21 21:58:22 -0400

    Added sadCycle solution

---
+++ sadCycle.java
@@ -0,0 +1,21 @@
+static void sadCycle(long base, long num) {
+    long temp;
+    List<Long> list = new ArrayList();
+    while (true) {
+        temp = 0;
+        while (num > 0) {
+            temp += Math.pow(num % 10, base);
+            num /= 10;
+        }
+        num = temp;
+        if (list.contains(num)) {
+            while (list.get(0) != num) {
+                list.remove(0);
+            }
+            System.out.println(list.toString());
+            break;
+        } else {
+            list.add(num);
+        }
+    }
+}
