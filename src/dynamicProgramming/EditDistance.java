d7dd1a8facb699cac53aab373a89f033468c2698
Josh Wein <jdog90000@yahoo.com>
2016-11-21 21:12:49 -0500

    Updated EditDistance

--- src/dynamicProgramming/EditDistance.java
+++ src/dynamicProgramming/EditDistance.java
@@ -2,31 +2,31 @@ package dynamicProgramming;
 
 /**
  * Created by Josh Wein on 11/20/2016.
+ * Implementation based of off pg 176: https://people.eecs.berkeley.edu/~vazirani/algorithms/chap6.pdf
  */
 public class EditDistance {
 
     public static void getEditDistance(String str1, String str2) {
         int[][] resultsTable = new int[str1.length() + 1][str2.length() + 1];
-
-        for (int i = 0; i <= str1.length(); i++) {
-            for (int j = 0; j <= str2.length(); j++) {
-                if (i == 0) {
-                    resultsTable[i][j] = j;
-                } else if (j == 0) {
-                    resultsTable[i][j] = i;
-                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) { // match
+        // for i = 0, 1, 2, . . . , m:
+        //   E(i, 0) = i
+        for (int i = 0; i < str1.length(); i++) {
+            resultsTable[i][0] = i;
+        }
+        // for j = 1, 2, . . . , n:
+        //   E(0, j) = j
+        for (int j = 1; j < str2.length(); j++) {
+            resultsTable[0][j] = j;
+        }
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) { // match
                     resultsTable[i][j] = resultsTable[i - 1][j - 1];
                 } else {
-                    int insert = resultsTable[i][j - 1];
-                    int delete = resultsTable[i - 1][j];
-                    int substitute = resultsTable[i - 1][j - 1];
-                    if (insert < delete && insert < substitute) {
-                        resultsTable[i][j] = insert + 1;
-                    } else if (delete < insert && delete < substitute) {
-                        resultsTable[i][j] = delete + 1;
-                    } else {
-                        resultsTable[i][j] = substitute + 1;
-                    }
+                    // E(i, j) = min{1 + E(i − 1, j), 1 + E(i, j − 1), diff(i, j) + E(i − 1, j − 1)}
+                    // diff(i, j) is defined to be 0 if x[i] = y[j] and 1 otherwise.
+                    int substitute = str1.charAt(i - 1) == str2.charAt(j - 1) ? resultsTable[i - 1][j - 1] : resultsTable[i - 1][j - 1] + 1;
+                    resultsTable[i][j] = min(resultsTable[i][j-1] + 1, resultsTable[i-1][j] + 1, substitute);
                 }
             }
         }
@@ -34,6 +34,16 @@ public class EditDistance {
         System.out.print("Path: ");
         reconstructPath(resultsTable, str1.length(), str2.length());
         System.out.println();
+    }
+
+    private static int min(int x, int y, int z) {
+        if (x < y && x < z) {
+            return x;
+        } else if (y < x && y < z) {
+            return y;
+        } else {
+            return z;
+        }
     }
 
     private static void reconstructPath(int[][] resultsTable, int i, int j) {
