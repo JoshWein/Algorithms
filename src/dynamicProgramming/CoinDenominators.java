2f25086e8417b201d52e86c790de807d3a0bac5e
Josh Wein <jdog90000@yahoo.com>
2016-12-04 16:33:29 -0500

    Implemented generating all possible ways to make change

--- src/dynamicProgramming/CoinDenominators.java
+++ src/dynamicProgramming/CoinDenominators.java
@@ -1,14 +1,22 @@
 package dynamicProgramming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Josh Wein on 11/20/2016.
  * Problem Statement: Given a set of coin denominators, find the minimum number of coins to make a certain amount of change.
  */
 public class CoinDenominators {
     public static void main(String[] args) {
-        int change = 50;
-        int[] coinDenominations = {1, 5, 10, 25};
+        int change = 20;
+        int[] coinDenominations = {1, 6, 10};
         System.out.println(calculateMinCoinsNeeded(change, coinDenominations));
+        ArrayList<Integer> denoms = new ArrayList<>();
+        for (int x : coinDenominations) denoms.add(x);
+        int possibilites = 0;
+        System.out.println("All ways to make " + change + " out of " + denoms.toString());
+        calculateAllWaysToMakeChange(change, denoms, new ArrayList<>(), possibilites);
     }
 
     private static int calculateMinCoinsNeeded(int totalChangeNeeded, int[] coinDenominations) {
@@ -26,5 +34,29 @@ public class CoinDenominators {
             }
         }
         return minCoinsNeeded[minCoinsNeeded.length - 1];
+    }
+
+    private static int calculateAllWaysToMakeChange(int totalChangeNeeded, ArrayList<Integer> coinDenominations, ArrayList<Integer> partial, int possibilities) {
+        int s = 0;
+        for (int x : partial) {
+            s += x;
+        }
+        if (s == totalChangeNeeded) {
+            possibilities++;
+            System.out.println(possibilities + ": " + Arrays.toString(partial.toArray()));
+        }
+        if (s >= totalChangeNeeded) {
+            return possibilities;
+        }
+        for (int i = 0; i < coinDenominations.size(); i++) {
+            ArrayList<Integer> remaining = new ArrayList<>();
+            for (int j = i; j < coinDenominations.size(); j++) {
+                remaining.add(coinDenominations.get(j));
+            }
+            ArrayList<Integer> curPartial = new ArrayList<>(partial);
+            curPartial.add(coinDenominations.get(i));
+            possibilities = calculateAllWaysToMakeChange(totalChangeNeeded, remaining, curPartial, possibilities);
+        }
+        return possibilities;
     }
 }
